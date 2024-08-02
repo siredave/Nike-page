5b2dea160f5c89bff47c40e4d7a88100f03fc005
siredave <siredave1@gmail.com>
2024-08-02 18:51:20 +0100

    steady update

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,15 @@
 import {services} from "../constants"
+import { ServiceCard } from "../components/ServiceCard"
+
 export const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
-        {sevices.map((service) => (
-          <ServiceCard key={service.label} />
+        {services.map((service) => (
+          <ServiceCard key={service.label}   
+          imgURL={service.imgURL}
+          label={service.label}
+          subtext={service.subtext}
+          />
         ))}
     </section>
   )
